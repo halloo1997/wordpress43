ac2beb612ac300ebe5c596ef72f548898b8fec08
chariesa <chariesa@hotmail.com>
2018-11-06 21:50:25 +0100

    Initial commit

---
+++ wp-content/themes/startwordpress/header.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo get_bloginfo( 'template_directory' );?>/blog.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <?php wp_head();?>
+
+</head>
+
+<body>
+
+<div class="blog-masthead">
+    <div class="container">
+        <nav class="blog-nav">
+            <?php wp_list_pages( '&title_li=' ); ?>
+        </nav>
+    </div>
+</div>
+
+<div class="container">
+
+    <div class="blog-header">
+        <h1 class="blog-title"><img src="http://15982.hosts1.ma-cloud.nl/wp-content/themes/startwordpress/img/logo.png" height="60px"/><a href="<?php echo get_bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
+        <p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p>
+        <div class="header">
+            <img src="http://15982.hosts1.ma-cloud.nl/wp-content/themes/startwordpress/img/getty92291965-56a02dd63df78cafdaa06d0f.jpg"/>
+
+<hr>
+        <div class="content">
+            <h1>Work</h1>
+            <div class="grid-container">
+                <div class="item1">
+                    <p class="category" style="position: absolute;right: 0;top: 0;background-color: #333;color: white!important;text-transform: capitalize;">category</p><img src="http://15982.hosts1.ma-cloud.nl/wp-content/themes/startwordpress/img/getty92291965-56a02dd63df78cafdaa06d0f.jpg" width="50%"/>
+                    <br>
+                    <p>Work example 3</p></div>
+                <div class="item2">
+                    <p class="category" style="position: absolute;right: 0;top: 0;background-color: #333;color: white!important;text-transform: capitalize;">category</p><img src="http://15982.hosts1.ma-cloud.nl/wp-content/themes/startwordpress/img/getty92291965-56a02dd63df78cafdaa06d0f.jpg"width="50%"/>
+                    <br>
+                    <p>Work example 6</p></div>
+                <div class="item3">
+                    <p class="category" style="position: absolute;right: 0;top: 0;background-color: #333;color: white!important;text-transform: capitalize;">category</p><img src="http://15982.hosts1.ma-cloud.nl/wp-content/themes/startwordpress/img/getty92291965-56a02dd63df78cafdaa06d0f.jpg"width="50%"/>
+                    <br>
+                    <p>Work example 1</p></div>
+                <div class="item4">
+                    <p class="category" style="position: absolute;right: 0;top: 0;background-color: #333;color: white!important;text-transform: capitalize;">category</p><img src="http://15982.hosts1.ma-cloud.nl/wp-content/themes/startwordpress/img/getty92291965-56a02dd63df78cafdaa06d0f.jpg" width="50%"/>
+                    <br>
+                    <p>Work example 2</p></div>
+                <div class="item5">
+                    <p class="category" style="position: absolute;right: 0;top: 0;background-color: #333;color: white!important;text-transform: capitalize;">category</p><img src="http://15982.hosts1.ma-cloud.nl/wp-content/themes/startwordpress/img/getty92291965-56a02dd63df78cafdaa06d0f.jpg"width="50%"/>
+                    <br>
+                    <p>Work example 4</p></div>
+                <div class="item6">
+                    <p class="category" style="position: absolute;right: 0;top: 0;background-color: #333;color: white!important;text-transform: capitalize;">category</p><img src="http://15982.hosts1.ma-cloud.nl/wp-content/themes/startwordpress/img/getty92291965-56a02dd63df78cafdaa06d0f.jpg"width="50%"/>
+                    <br>
+                    <p>Work example 5</p></div>
+            </div>
+            <hr>
+            <h1>News</h1>
+            <div class="grid-container">
+                <div class="item1">
+                    <p class="category" style="position: absolute;right: 0;top: 0;background-color: #333;color: white!important;text-transform: capitalize;">date</p><img src="http://15982.hosts1.ma-cloud.nl/wp-content/themes/startwordpress/img/getty92291965-56a02dd63df78cafdaa06d0f.jpg" width="100%"/>
+                    <br>
+                    <p>Work example 3</p></div>
+                <div class="item2">
+                    <p class="category" style="position: absolute;right: 0;top: 0;background-color: #333;color: white!important;text-transform: capitalize;">date</p><img src="http://15982.hosts1.ma-cloud.nl/wp-content/themes/startwordpress/img/getty92291965-56a02dd63df78cafdaa06d0f.jpg"width="100%"/>
+                    <br>
+                    <p>Work example 6</p></div>
+                <div class="item3">
+                    <p class="category" style="position: absolute;right: 0;top: 0;background-color: #333;color: white!important;text-transform: capitalize;">date</p><img src="http://15982.hosts1.ma-cloud.nl/wp-content/themes/startwordpress/img/getty92291965-56a02dd63df78cafdaa06d0f.jpg"width="100%"/>
+                    <br>
+                    <p>Work example 1</p></div>
+<!--                <div class="item4">-->
+<!--                    <p class="category" style="position: absolute;right: 0;top: 0;background-color: #333;color: white!important;text-transform: capitalize;">category</p>>category</p><img src="http://15982.hosts1.ma-cloud.nl/wp-content/themes/startwordpress/img/getty92291965-56a02dd63df78cafdaa06d0f.jpg" width="100%"/>-->
+<!--                    <br>-->
+
+<!--                </div>-->
+    </div>
